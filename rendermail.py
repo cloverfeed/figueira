64f19380e7d38b4a5ea03199151786a0a5381284
Etienne Millon <me@emillon.org>
2014-03-02 20:33:48 +0100

    mini template

--- rendermail.py
+++ rendermail.py
@@ -1,10 +1,27 @@
-import sys
+from mako.template import Template
+import email
+import fileinput
+
 
 def main():
-    lines = sys.stdin.readlines()
+    lines = []
+    for line in fileinput.input():
+        lines.append(line)
     s = ''.join(lines)
-    print s
 
+    msg = email.message_from_string(s)
+
+    part = msg.get_payload()[1]
+    charset = part.get_content_charset()
+    body = part.get_payload(decode=True).decode(charset)
+
+    temp = Template(filename='templates/message.mako', default_filters=['h'])
+    out = temp.render(_from=msg['From'],
+                      date=msg['Date'],
+                      subject=msg['Subject'],
+                      body=body,
+                      )
+    print out.encode('utf8')
 
 if __name__ == '__main__':
     main()
---
+++ templates/message.mako
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<div class="message">
+<div class="from">${_from}</div>
+<div class="date">${date}</div>
+<div class="subject">${subject}</div>
+<div class="body">${body|n}</div>
+</div>
+
+</body>
+</html>
